4bb22cb71db1585de0f1a98d51f5cabe4aa4b87f
fedelanyt130 <federicoyt150@gmail.com>
2025-06-20 17:02:45 -0300

    Update fun-top.js

--- plugins/fun-top.js
+++ plugins/fun-top.js
@@ -2,7 +2,7 @@ import util from 'util'
 import path from 'path'
 let user = a => '@' + a.split('@')[0]
 function handler(m, { groupMetadata, command, conn, text, usedPrefix}) {
-if (!text) return conn.reply(m.chat, `*${xfun} Por favor, ingresa un texto.*\n> *\`Ejemplo:\`* ${usedPrefix + command} Negros`, m)
+if (!text) return conn.reply(m.chat, `${emoji} Por favor, ingrese un texto para hacer un Top 10 *texto*.`, m)
 let ps = groupMetadata.participants.map(v => v.id)
 let a = ps.getRandom()
 let b = ps.getRandom()
@@ -35,7 +35,10 @@ m.reply(top, null, { mentions: [a, b, c, d, e, f, g, h, i, j]})
 handler.help = ['top *<texto>*']
 handler.command = ['top']
 handler.tags = ['fun']
-handler.group = true
+handler.group = true;
+handler.register = true
+
 export default handler
+
 function pickRandom(list) {
 return list[Math.floor(Math.random() * list.length)]}
