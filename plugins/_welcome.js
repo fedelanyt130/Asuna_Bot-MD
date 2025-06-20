d332b550a457556b9f0abe250cc604a80edf2323
fedelanyt130 <federicoyt150@gmail.com>
2025-06-20 15:13:13 -0300

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -4,19 +4,6 @@
 
 import { WAMessageStubType } from '@whiskeysockets/baileys'
 
-const paises = {
-  "1": "🇺🇸 Estados Unidos", "34": "🇪🇸 España", "52": "🇲🇽 México", "54": "🇦🇷 Argentina",
-  "55": "🇧🇷 Brasil", "56": "🇨🇱 Chile", "57": "🇨🇴 Colombia", "58": "🇻🇪 Venezuela",
-  "591": "🇧🇴 Bolivia", "593": "🇪🇨 Ecuador", "595": "🇵🇾 Paraguay", "598": "🇺🇾 Uruguay",
-  "502": "🇬🇹 Guatemala", "503": "🇸🇻 El Salvador", "504": "🇭🇳 Honduras", "505": "🇳🇮 Nicaragua",
-  "506": "🇨🇷 Costa Rica", "507": "🇵🇦 Panamá", "51": "🇵🇪 Perú", "53": "🇨🇺 Cuba", "91": "🇮🇳 India"
-};
-
-function obtenerPais(numero) {
-  let num = numero.replace("@s.whatsapp.net", "");
-  let codigo = Object.keys(paises).find(pref => num.startsWith(pref));
-  return paises[codigo] || "🌐 Desconocido";
-}
 
 export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return;
@@ -26,9 +13,6 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let taguser = `@${who.split("@")[0]}`;
   let chat = global.db.data.chats[m.chat];
   let totalMembers = participants.length;
-  let date = new Date().toLocaleString("es-ES", { timeZone: "America/Mexico_City" });
-
-  let pais = obtenerPais(who);
   let ppUser = 'https://files.catbox.moe/h1eizu.jpg';
 
   try {
@@ -60,7 +44,6 @@ export async function before(m, { conn, participants, groupMetadata }) {
       let bienvenida = `
 *💖─『 𝑩𝑰𝑬𝑵𝑽𝑬𝑵𝑰𝑫𝑶/𝑨 』─✨*
 👤 *Usuario:* ${taguser}
-🌍 *País:* ${pais}
 💬 *Grupo:* *${groupMetadata.subject}*
 👥 *Miembros:* *${totalMembers + 1}*
 📅 *Fecha:* *${date}*
@@ -81,7 +64,6 @@ export async function before(m, { conn, participants, groupMetadata }) {
       let despedida = `
 *💖──『 𝑫𝑬𝑺𝑷𝑬𝑫𝑰𝑫𝑨 』──✨*
 👤 *Usuario:* ${taguser}
-🌍 *País:* ${pais}
 💬 *Grupo:* *${groupMetadata.subject}*
 👥 *Miembros:* *${totalMembers - 1}*
 📅 *Fecha:* *${date}*
