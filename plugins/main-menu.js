006df5192dd82afffcfb9033cd4bb7ccc2c217e2
fedelanyt130 <federicoyt150@gmail.com>
2025-06-20 10:02:35 -0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -24,8 +24,8 @@ const defaultMenu = {
 ╰─❒ 𝗔𝗦𝗨𝗡𝗔 – 𝗕𝗢𝗧 ❒─╯
 
 ╭─❒「 𝗘𝗦𝗧𝗔𝗗𝗢 𝗗𝗘 𝗨𝗦𝗨𝗔𝗥𝗜𝗢 」
-│ 🏷 COMANDOS: %totalCommands
-│ 📊 𝗡𝗜𝗩𝗘𝗟: %level
+│ 🏷 COMANDOS: ${totalCommands}
+│ 📊 𝗡𝗜𝗩𝗘𝗟: ${level}
 │ ⚡ 𝗘𝗫𝗣: %exp / %maxexp
 │ 👤 𝗨𝗦𝗨𝗔𝗥𝗜𝗢𝗦: %totalreg
 │ ⏱ 𝗧𝗜𝗘𝗠𝗣𝗢 𝗔𝗖𝗧𝗜𝗩𝗢: %muptime
