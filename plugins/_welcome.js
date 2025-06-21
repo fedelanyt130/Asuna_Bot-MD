c905acadcc332510b4421e041cbc44bc5dd3d9ba
fedelanyt130 <federicoyt150@gmail.com>
2025-06-21 13:35:26 -0300

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -7,7 +7,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let who = m.messageStubParameters[0]
   let taguser = `@${who.split('@')[0]}`
   let chat = global.db.data.chats[m.chat]
-  let defaultImage = 'https://files.catbox.moe/k4cdwk.jpg';
+  let defaultImage = 'https://files.catbox.moe/h1eizu.jpg';
 
   if (chat.welcome) {
     let img;
