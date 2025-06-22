b8d441f0a2bb548c38e41d7b2d75c7108c6cafff
fedelanyt130 <federicoyt150@gmail.com>
2025-06-22 18:04:58 -0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -23,7 +23,7 @@ let tags = {
   'logos': 'EDICION LOGOS', 
 }
 
-const vid = 'https://cdnmega.vercel.app/media/dwx0CKRD@MmwtDrN7W6x4EIFtt4ss50UJpk-F2fFXJBueIW1IZR8';
+const vid = 'https://files.catbox.moe/2ovhhs.mp4';
 
 const defaultMenu = {
   before: `*✩───────────✩*
