70b72edfc97bc37691d6b86f9cbdc036fd68e379
fedelanyt130 <federicoyt150@gmail.com>
2025-06-22 18:06:06 -0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -149,8 +149,6 @@ await m.react(emojis)
 /* await conn.sendMessage(m.chat, { video: { url: vid }, caption: text.trim(), contextInfo: { mentionedJid: [m.sender], isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, newsletterName: channelRD.name, serverMessageId: -1, }, forwardingScore: 999, externalAdReply: { title: textbot, body: dev, thumbnailUrl: 'https://qu.ax/kJBTp.jpg', sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false,
 }, }, gifPlayback: true, gifAttribution: 0 }, { quoted: null }) */
 
-let img = 'https://github.com/fedelanyt130.png'; // valiendo vrg con los links
-
   await conn.sendMessage(m.chat, { 
       text: text.trim(),
       contextInfo: {
@@ -165,7 +163,7 @@ let img = 'https://github.com/fedelanyt130.png'; // valiendo vrg con los links
           externalAdReply: {
               title: textbot,
               body: dev,
-              thumbnailUrl: img,
+              thumbnailUrl: vid,
               sourceUrl: false,
               mediaType: 1,
               showAdAttribution: true,
