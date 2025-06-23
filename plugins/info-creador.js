5e50b893856f6b1c0a28b4bb8438726f72332b7f
fedelanyt130 <federicoyt150@gmail.com>
2025-06-23 18:52:30 -0300

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,62 +1,74 @@
-import PhoneNumber from 'awesome-phonenumber'
+// Código creado por Deylin
+// https://github.com/Deylin-eliac 
+// codigo creado para https://github.com/Deylin-eliac/Pikachu-bot 
+// No quites créditos
 
-let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-//m.react('⚙️')
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
-let biografia = await conn.fetchStatus('5218711526787' +'@s.whatsapp.net').catch(_ => 'Sin Biografia')
-let biografiaBot = await conn.fetchStatus(`${conn.user.jid.split('@')[0]}` +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
-let bio = biografia.status?.toString() || 'Sin Biografía'
-let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
-let name = await conn.getName(who)
+import PhoneNumber from 'awesome-phonenumber';
 
-  await sendContactArray(conn, m.chat, [
-     [`${nomorown}`, `❤️‍🔥 Propietario`, `★彡( Ӿł_₥ł₲ɄɆⱠØ₦77ӾӾ )彡★`, dev, correo, `MX`, `${global.yt}`, bio],
-[`${conn.user.jid.split('@')[0]}`, `Es Una Bot 🦋`, `${packname}`, `📵 No Hacer Spam`, correo, `MX`, md, biobot]
-], m)
-  //m.reply(`Hola @${m.sender.split(`@`)[0]} este es el contacto de mi creador, no hagas spam!!`)
-  } 
+let handler = async (m, { conn }) => {
+  m.react('👤');
+  
+const imageUrl = 'https://files.catbox.moe/hmvsyj.jpg'
+  const numCreador = '5491156178758';
+  const ownerJid = numCreador + '@s.whatsapp.net';
+  const name = await conn.getName(ownerJid) || 'Fede';
+  const about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || `һ᥆ᥣᥲ¡ s᥆ᥡ 𝖿ᥱძᥱ mᥙᥴһ᥆ gᥙs𝗍᥆.`;
+  const empresa = 'fede - Servicios Tecnológicos';
 
-handler.help = ["creador","owner"]
-handler.tags = ["info"]
-handler.command = ['owner','creador']
-export default handler
-
-async function sendContactArray(conn, jid, data, quoted, options) {
-        if (!Array.isArray(data[0]) && typeof data[0] === 'string') data = [data]
-                let contacts = []
-        for (let [number, name, isi, isi1, isi2, isi3, isi4, isi5] of data) {
-            number = number.replace(/[^0-9]/g, '')
-            let njid = number + '@s.whatsapp.net'
-            let biz = await conn.getBusinessProfile(njid).catch(_ => null) || {}
-            // N:;${name.replace(/\n/g, '\\n').split(' ').reverse().join(';')};;;
-            let vcard = `
+  const vcard = `
 BEGIN:VCARD
 VERSION:3.0
-N:Sy;Bot;;;
-FN:${name.replace(/\n/g, '\\n')}
-item.ORG:${isi}
-item1.TEL;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}
-item1.X-ABLabel:${isi1}
-item2.EMAIL;type=INTERNET:${isi2}
-item2.X-ABLabel:📧 Email
-item3.ADR:;;${isi3};;;;
-item3.X-ABADR:ac
-item3.X-ABLabel:🏷 Region
-item4.URL:${isi4}
-item4.X-ABLabel:Website
-item5.X-ABLabel:${isi5}
-END:VCARD`.trim()
-            contacts.push({ vcard, displayName: name })
-        }
-        return await conn.sendMessage(jid, {
-            contacts: {
-                displayName: (contacts.length > 1 ? `2013 kontak` : contacts[0].displayName) || null,
-                contacts,
-            }
+N:;${name};;;
+FN:${name}
+ORG:${empresa};
+TITLE:CEO & Fundador
+TEL;waid=${numCreador}:${new PhoneNumber('+' + numCreador).getNumber('international')}
+EMAIL:correo@empresa.com
+URL:https://www.tuempresa.com
+NOTE:${about}
+ADR:;;Dirección de tu empresa;;;;
+X-ABADR:ES
+X-ABLabel:Dirección Web
+X-ABLabel:Correo Electrónico
+X-ABLabel:Teléfono de contacto
+X-WA-BIZ-NAME:${name}
+X-WA-BIZ-DESCRIPTION:${about}
+END:VCARD`.trim();
+
+
+  await conn.sendMessage(
+    m.chat,
+    {
+      contacts: {
+        displayName: name,
+        contacts: [{ vcard }]
+      },
+      contextInfo: {
+        mentionedJid: [m.sender],
+        isForwarded: true,
+        forwardingScore: 999,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: channelRD.id,
+          newsletterName: channelRD.name,
+          serverMessageId: -1,
         },
-        {
-            quoted,
-            ...options
-        })
+        externalAdReply: {
+          title: textbot,
+          body: dev,
+          thumbnailUrl: imageUrl,
+          sourceUrl: redes,
+          mediaType: 1,
+          showAdAttribution: true,
+          renderLargerThumbnail: true,
+        },
+      }
+    },
+    { quoted: m }
+  );
 }
+
+handler.help = ['owner'];
+handler.tags = ['main'];
+handler.command = ['owner', 'creator', 'creador', 'dueño'];
+
+export default handler;
