e14e74381c80e096323715d8b57c2c87ad8ea0b9
fedelanyt130 <federicoyt150@gmail.com>
2025-06-23 11:59:44 -0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -40,8 +40,8 @@ const defaultMenu = {
 %readmore
 \t*(✰◠‿◠) ᴀꜱᴜɴᴀʙᴏᴛ-ᴀʟ*   
 `.trimStart(),
-  header: '✩──\n %category \n──✩',
-  body: '*❒* %cmd',
+  header: '💖──\n %category \n──💖',
+  body: '*◌❐🎋༉* %cmd',
   footer: '*ᴀꜱᴜɴᴀʙᴏᴛ-ᴀʟ*\n',
   after: `> ${dev}`,
 }
