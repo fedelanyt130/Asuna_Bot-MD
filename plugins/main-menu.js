a9567a99390fa4792eb2a7fd2874b635bd88ed3d
fedelanyt130 <federicoyt150@gmail.com>
2025-06-23 12:10:07 -0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -42,7 +42,7 @@ const defaultMenu = {
 `.trimStart(),
   header: '\n `%category` \n',
   body: '*◌❐💖༉* %cmd',
-  footer: '*♡─────♡*\n',
+  footer: '**\n',
   after: `> ${dev}`,
 }
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
